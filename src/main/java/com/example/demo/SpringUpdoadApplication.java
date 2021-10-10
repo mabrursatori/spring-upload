8df3e955ecc5786ebd0976b0acb07f133a0e3b1c
mabrursatori <mabrursatori@gmail.com>
2021-10-10 17:26:58 +0700

    initial commit

---
+++ src/main/java/com/example/demo/SpringUpdoadApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUpdoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUpdoadApplication.class, args);
+	}
+
+}
